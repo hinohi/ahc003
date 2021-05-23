7acde4565044f481353e742c6a1853637abb039c
Nakayama Daiju <42.daiju@gmail.com>
2021-05-23 13:29:32 +0900

    street 全てを更新する 91,887,958,902

--- solver/src/naive_local.rs
+++ solver/src/naive_local.rs
@@ -52,7 +52,7 @@ impl NaiveLocalSolver {
             pos: start,
             path: Vec::new(),
         });
-        let mut visited = [[1e100; N]; N];
+        let mut visited = [[1e300; N]; N];
         while let Some(state) = heap.pop() {
             if state.pos == end {
                 return (state.dist, state.path);
@@ -102,28 +102,54 @@ impl NaiveLocalSolver {
         let query = judge.next_query();
         let (dist, path) = self.calc_path((query[0], query[1]), (query[2], query[3]));
         judge.answer(&dir_to_str(&path));
-        let avg = judge.path_length() as f64 / dist;
+        let length = judge.path_length() as f64;
+
         let mut y = query[0];
         let mut x = query[1];
-        let p = 0.8;
-        for d in path {
+        let mut st_v = [(0.0, 0); N];
+        let mut st_h = [(0.0, 0); N];
+        for &d in path.iter() {
             match d {
                 Direction::U => {
                     y -= 1;
-                    self.vertical[y][x] = mix(p, self.vertical[y][x], self.vertical[y][x] * avg);
+                    st_v[x].0 += self.vertical[y][x];
+                    st_v[x].1 += 1;
                 }
                 Direction::D => {
-                    self.vertical[y][x] = mix(p, self.vertical[y][x], self.vertical[y][x] * avg);
-                    y += 1;
+                    st_v[x].0 += self.vertical[y][x];
+                    st_v[x].1 += 1;
+                    y += 1
                 }
                 Direction::L => {
                     x -= 1;
-                    self.horizon[y][x] = mix(p, self.horizon[y][x], self.horizon[y][x] * avg);
+                    st_h[y].0 += self.horizon[y][x];
+                    st_h[y].1 += 1;
                 }
                 Direction::R => {
-                    self.horizon[y][x] = mix(p, self.horizon[y][x], self.horizon[y][x] * avg);
+                    st_h[y].0 += self.horizon[y][x];
+                    st_h[y].1 += 1;
                     x += 1;
                 }
+            }
+        }
+
+        let p: f64 = 0.4;
+        for (x, &(st_len, count)) in st_v.iter().enumerate() {
+            if count == 0 {
+                continue;
+            }
+            let avg = length * st_len / dist / count as f64;
+            for y in 0..N - 1 {
+                self.vertical[y][x] = mix(p, self.vertical[y][x], avg);
+            }
+        }
+        for (y, &(st_len, count)) in st_h.iter().enumerate() {
+            if count == 0 {
+                continue;
+            }
+            let avg = length * st_len / dist / count as f64;
+            for x in 0..N - 1 {
+                self.horizon[y][x] = mix(p, self.horizon[y][x], avg);
             }
         }
     }
