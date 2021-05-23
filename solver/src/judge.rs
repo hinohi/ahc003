1b2614f0f6cef14e2b97c5e210cd0197f050557f
Nakayama Daiju <42.daiju@gmail.com>
2021-05-24 02:55:48 +0900

    judge をインライン化して高速化？

--- solver/src/judge.rs
+++ solver/src/judge.rs
@@ -1,4 +1,7 @@
-use std::io::{stdin, stdout, Write};
+#[cfg(feature = "internal_tester")]
+pub type BestJudge = InternalJudge;
+#[cfg(not(feature = "internal_tester"))]
+pub type BestJudge = OuterJudge;
 
 pub trait Judge {
     fn next_query(&mut self) -> [usize; 4];
@@ -6,12 +9,27 @@ pub trait Judge {
     fn answer(&mut self, ans: &str);
 
     fn path_length(&mut self) -> u32;
+
+    fn score(&self) -> i64 {
+        0
+    }
 }
 
+#[cfg(not(feature = "internal_tester"))]
 pub struct OuterJudge;
 
+#[cfg(not(feature = "internal_tester"))]
+impl OuterJudge {
+    pub fn new(_: &str) -> OuterJudge {
+        OuterJudge
+    }
+}
+
+#[cfg(not(feature = "internal_tester"))]
 impl Judge for OuterJudge {
     fn next_query(&mut self) -> [usize; 4] {
+        use std::io::stdin;
+
         let stdin = stdin();
         let mut buf = String::with_capacity(11);
         stdin.read_line(&mut buf).expect("fail to read query");
@@ -41,15 +59,69 @@ impl Judge for OuterJudge {
     }
 
     fn answer(&mut self, ans: &str) {
+        use std::io::{stdout, Write};
+
         let mut stdout = stdout();
         writeln!(stdout, "{}", ans).expect("fail to write answer to stdout");
         stdout.flush().expect("fail to flush stdout");
     }
 
     fn path_length(&mut self) -> u32 {
+        use std::io::stdin;
+
         let stdin = stdin();
         let mut buf = String::new();
         stdin.read_line(&mut buf).expect("fail to read path length");
         buf.trim().parse().expect("not int at path length")
+    }
+}
+
+#[cfg(feature = "internal_tester")]
+pub struct InternalJudge {
+    input: tools::Input,
+    outs: Vec<String>,
+    visited: Vec<Vec<usize>>,
+}
+
+#[cfg(feature = "internal_tester")]
+impl InternalJudge {
+    pub fn new(input: &str) -> InternalJudge {
+        use crate::consts::N;
+        use tools::mat;
+
+        InternalJudge {
+            input: tools::read_input_str(input),
+            outs: Vec::new(),
+            visited: mat![!0; N; N],
+        }
+    }
+}
+
+#[cfg(feature = "internal_tester")]
+impl Judge for InternalJudge {
+    fn next_query(&mut self) -> [usize; 4] {
+        let i = self.outs.len();
+        let query = [
+            self.input.s[i].0,
+            self.input.s[i].1,
+            self.input.t[i].0,
+            self.input.t[i].1,
+        ];
+        query
+    }
+
+    fn answer(&mut self, ans: &str) {
+        self.outs.push(ans.to_owned());
+    }
+
+    fn path_length(&mut self) -> u32 {
+        let k = self.outs.len() - 1;
+        tools::compute_path_length(&self.input, k, self.outs.last().unwrap(), &mut self.visited)
+            .unwrap()
+            .0 as u32
+    }
+
+    fn score(&self) -> i64 {
+        tools::compute_score_detail(&self.input, &self.outs).0
     }
 }
--- solver/src/main.rs
+++ solver/src/main.rs
@@ -5,15 +5,25 @@ mod naive_local;
 mod naive_manhattan;
 mod utils;
 
-use judge::OuterJudge;
-use naive_local::NaiveLocalSolver;
+use std::{env::args, fs};
+
+use crate::judge::{BestJudge, Judge};
+use crate::naive_local::NaiveLocalSolver;
 #[cfg(feature = "naive_manhattan")]
 use naive_manhattan::NaiveManhattan;
 
 fn main() {
-    let mut judge = OuterJudge;
+    let input = args()
+        .skip(1)
+        .next()
+        .map_or("".to_owned(), |path| fs::read_to_string(path).unwrap());
+    let mut judge = BestJudge::new(&input);
     let mut solver = NaiveLocalSolver::new();
     for _ in 0..crate::consts::Q {
         solver.solve(&mut judge);
+    }
+    let score = judge.score();
+    if score != 0 {
+        eprintln!("Score = {}", score);
     }
 }
